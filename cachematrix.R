063639cb67f5872c03def91c35613c1dc43dcbec
bandamaurya <43462369+bandamaurya@users.noreply.github.com>
2018-09-24 13:08:58 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -18,12 +18,13 @@ function(x = matrix()) {
   list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
+
 # The following function returns the inverse of the matrix. It first checks if
 # the inverse has already been computed. If so, it gets the result and skips the
 # computation. If not, it computes the inverse, sets the value in the cache via 
 # setinverse function
 
-+function(x, ...) {
+function(x, ...) {
   inv <- x$getinverse()
   if(!is.null(inv)) {
     message("getting cached data.")
